b338d339ab1639231c78399fef4bf611f4b8936e
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-30 22:14:12 +0100

    function working js

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
     window.addEventListener("load", eventListerner);
     window.addEventListener("scroll", eventListerner);
+
+    function eventListerner(){
+        
+    }
 });
