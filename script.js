9f258cd32de03e673d1a731f6f8f591f028a210c
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-30 22:13:18 +0100

    load and scroll method js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sections = document.querySelectorAll("section");
 
 sections.forEach((section) => {
-    
+    window.addEventListener("load", eventListerner);
+    window.addEventListener("scroll", eventListerner);
 });
