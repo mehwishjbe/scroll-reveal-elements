68ccda009c145ec2e40653cda6c964d36fc4f0f0
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-30 22:19:52 +0100

    declaring variables

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const sections = document.querySelectorAll("section");
 
 sections.forEach((section) => {
-    window.addEventListener("load", eventListerner);
-    window.addEventListener("scroll", eventListerner);
+  window.addEventListener("load", eventListerner);
+  window.addEventListener("scroll", eventListerner);
 
-    function eventListerner(){
-        
-    }
+  function eventListerner() {
+    let windowHeight = window.innerHeight;
+    let sectionRectTop = section.getBoundingClientRect().top;
+  }
 });
